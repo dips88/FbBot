fe6dff06e3c6ebd1de8677e96e1203d42606e5ca
DibyamAgrawal <dibyam.agrawal@gmail.com>
2017-02-26 11:45:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,9 +38,9 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-
-                    send_message(sender_id, "got it, thanks!")
-
+                    ##################################################
+                    newMsg(sender_id,message_text)
+                    ##################################################
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
 
@@ -52,6 +52,10 @@ def webhook():
 
     return "ok", 200
 
+def newMsg(recipient_id, message_text):
+    send_message(recipient_id, "got it, thanks!")
+    with open("test.txt", "a") as myfile:
+        myfile.write(recipient_id + ":" +message_text + "\n")
 
 def send_message(recipient_id, message_text):
 
